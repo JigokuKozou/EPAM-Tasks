c636027dc58ae93a3624f4bd305ad56959d716ae
Ultimate Escanor <secretplayer@inbox.ru>
2022-01-23 21:30:32 +0400

    Add Obstacle Stone

---
+++ Task 2/Task_2_2/Models/Obstacles/Stone.cs
@@ -0,0 +1,9 @@
+﻿using System.Drawing;
+
+namespace Task_2_2.Models.Obstacles
+{
+    public class Stone : Obstacle
+    {
+        public Stone(Point location) : base("Камень", location, '#', new(health: 100, armor: 50)) { }
+    }
+}
