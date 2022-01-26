e006f452a41d35e274d65109bd2539e7df5eceaa
Ultimate Escanor <secretplayer@inbox.ru>
2022-01-26 17:34:32 +0400

    Deleted IBonus

--- Task 2/Task_2_2/Interfaces/IBonus.cs
+++
@@ -1,9 +0,0 @@
-﻿using Task_2_2.Models;
-
-namespace Task_2_2.Interfaces
-{
-    public interface IBonus
-    {
-        Characteristics Characteristics { get; }
-    }
-}
--- Task 2/Task_2_2/Models/Items/Item.cs
+++ Task 2/Task_2_2/Models/Items/Item.cs
@@ -1,9 +1,8 @@
 ﻿using System.Drawing;
-using Task_2_2.Interfaces;
 
 namespace Task_2_2.Models.Items
 {
-    public abstract class Item : Entity, IBonus
+    public abstract class Item : Entity
     {
         protected Item(string name, Point location, char icon, Characteristics characteristics) :
             base(name, location, icon)
