6b833cbcac8d9659417e7ee3ae3e0a6076551ffe
Ultimate Escanor <secretplayer@inbox.ru>
2022-01-26 16:48:58 +0400

    Square now inherits from ClosedFigure

--- Task 2/Task_2/Figures/Square.cs
+++ Task 2/Task_2/Figures/Square.cs
@@ -1,9 +1,31 @@
-﻿namespace Task_1.Figures
+﻿using System.Text;
+
+namespace Task_1.Figures
 {
-    public class Square : Rectangle
+    public class Square : СlosedFigure
     {
-        public Square(Point location, double sideLength) : base(location, sideLength, sideLength) { }
+        public Square(Point center, double sideLength)
+        {
+            Center = center;
+            SideLength = sideLength;
+        }
+
+        public Point Center { get; }
+
+        public double SideLength { get; }
+
+        public override double Area => SideLength * SideLength;
+
+        public override double Perimeter => SideLength * 4;
 
         public override string ToString() => "Квадрат";
+
+        public override string GetInfo()
+        {
+            StringBuilder builder = new(base.GetInfo());
+            builder.AppendLine($"Центр в точке: {Center}");
+            builder.AppendLine($"Длинна стороны: {SideLength}");
+            return builder.ToString();
+        }
     }
 }
