770f4f1b2dde484da372244aff164d73af9907ac
Ultimate Escanor <secretplayer@inbox.ru>
2022-01-27 17:43:06 +0400

    Added Task 3.1.1

---
+++ Task 3/WeakestLink/Game.cs
@@ -0,0 +1,41 @@
+﻿namespace WeakestLink
+{
+    public class Game
+    {
+        private List<People> _peoples;
+
+        private int _offset;
+
+        public Game(int peopleAmount, int offset)
+        {
+            _peoples = new(peopleAmount);
+            for (int i = 0; i < peopleAmount; i++)
+            {
+                _peoples.Add(new People(i + 1));
+            }
+
+            _offset = offset;
+        }
+
+        public Action<int> OnRoundEnd = delegate { };
+
+        public Action OnGameEnd = delegate { };
+
+        public int PeoplesCount => _peoples.Count;
+
+        public void Start()
+        {
+            int removingIndex = _offset - 1;
+            for (int round = 1; _offset <= _peoples.Count; round++)
+            {
+                _peoples.Remove(_peoples[removingIndex]);
+                removingIndex += _offset - 1;
+                removingIndex %= _peoples.Count;
+
+                OnRoundEnd(round);
+            }
+
+            OnGameEnd();
+        }
+    }
+}
---
+++ Task 3/WeakestLink/People.cs
@@ -0,0 +1,12 @@
+﻿namespace WeakestLink
+{
+    internal struct People
+    {
+        public int ID { get; }
+
+        public People(int iD)
+        {
+            ID = iD;
+        }
+    }
+}
