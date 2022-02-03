02193b4e040af19b5119984b12dc2cd79ec4af91
Ultimate Escanor <secretplayer@inbox.ru>
2022-02-03 19:47:51 +0400

    Add Task 3.3.1 SuperArray

---
+++ Task 3/SuperArray/Program.cs
@@ -0,0 +1,8 @@
+﻿using SuperArray;
+
+int[] arr = { 1, 2, 4, 2 };
+arr.ForEach(x => Console.Write(x + " "));
+Console.WriteLine();
+Console.WriteLine("Sum elements: " + SuperArrayExtentions.Sum(arr));
+Console.WriteLine("Average value: " + SuperArrayExtentions.Average(arr));
+Console.WriteLine("Most repeated value: " + arr.FindMostRepeatedElement());
