3025b952dc1abb37977ccdafb5cd56ff4ee9ae42
Ultimate Escanor <secretplayer@inbox.ru>
2022-02-03 20:59:43 +0400

    Add Task 3.3.2

---
+++ Task 3/SuperString/Program.cs
@@ -0,0 +1,17 @@
+﻿using SuperString;
+
+Console.WriteLine("Примеры:");
+string example = "Русский";
+Console.WriteLine(example + " " + example.GetLanguage());
+
+example = "English";
+Console.WriteLine(example + " " + example.GetLanguage());
+
+example = "Mixed миксированный";
+Console.WriteLine(example + " " + example.GetLanguage());
+
+example = "Mixed,";
+Console.WriteLine(example + " " + example.GetLanguage());
+
+example = "Mixed ";
+Console.WriteLine(example + " " + example.GetLanguage());
