908194d3d3d2dd88df649551361f1f6e1950d757
Ultimate Escanor <secretplayer@inbox.ru>
2022-02-06 15:28:59 +0400

    Added Pizzeria, User, Order, Pizza, PizzaTypes

---
+++ Task 3/PizzaTime/User.cs
@@ -0,0 +1,53 @@
+﻿namespace PizzaTime
+{
+    public class User
+    {
+        private List<Pizza> _inventory = new();
+
+        private Pizzeria _pizzeria;
+
+        private List<Order> _transferedOrders = new();
+
+        private List<PizzaType> _pizzaTypes = new();
+
+        public User(Pizzeria pizzeria)
+        {
+            _pizzeria = pizzeria;
+        }
+
+        public IEnumerable<Order> TransferedOrders => _transferedOrders;
+
+        public IEnumerable<PizzaType> CurrentPizzaTypes => _pizzaTypes;
+
+        public IEnumerable<Pizza> Inventory => _inventory;
+
+        public void AddPizzaToOrder(PizzaType pizzaType) => _pizzaTypes.Add(pizzaType);
+
+        public bool RemovePizzaFromOrder(PizzaType pizzaType) => _pizzaTypes.Remove(pizzaType);
+
+        public void TransferOrder()
+        {
+            if (_pizzaTypes.Count == 0)
+                return;
+
+            var order = new Order(_pizzaTypes);
+            _pizzaTypes.Clear();
+
+            _pizzeria.TakeOrder(order);
+            _transferedOrders.Add(order);
+        }
+
+        public void PickUpOrder()
+        {
+            foreach (var order in _transferedOrders)
+            {
+                if (_pizzeria.CompletedOrders.Contains(order.ID))
+                {
+                    _pizzeria.TryGetPizza(order, out IEnumerable<Pizza> receivedPizza);
+                    _inventory.AddRange(receivedPizza);
+                    return;
+                }
+            }
+        }
+    }
+}
