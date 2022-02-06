c3fc1eac1d4b241c96020e73773b45861d0b1886
Ultimate Escanor <secretplayer@inbox.ru>
2022-02-06 15:29:42 +0400

    Add Table, ConsoleUI, UserUI

---
+++ Task 3/PizzaTime/UserUI.cs
@@ -0,0 +1,15 @@
+﻿namespace PizzaTime
+{
+    public sealed class UserUI
+    {
+        private List<string> _messages = new();
+
+        public void AddLine(string message) => _messages.Add(message);
+
+        public void Clear() => _messages.Clear();
+
+        public void PrintMessages() => Console.WriteLine(GetMessages());
+
+        public string GetMessages() => string.Join(Environment.NewLine, _messages);
+    }
+}
